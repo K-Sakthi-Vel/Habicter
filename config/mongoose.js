38f581ba9226167201cfd6a1cdb3f0ceb1353c14
K SAKTHIVEL <klsakthi333@gmail.com>
2024-05-23 11:23:31 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,17 +1,43 @@
-// require mongoose
 const mongoose = require('mongoose');
-// connect to database and collection
-mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+//fill your database name here
+
+mongoose.connect(
+  'mongodb+srv://klsakthi3344:klsakthi333@cluster0.b9ziqxt.mongodb.net/',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
-// in case of any error in connection
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on('error', console.error.bind('console', 'error'));
 
-// tell us once connection with database was successful
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once('open', function () {
+  console.log('welcome connect to database');
 });
 
-// exporting and make available
 module.exports = db;
+
+
+
+
+
+
+// require mongoose
+// const mongoose = require('mongoose');
+// connect to database and collection
+// mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+
+// const db = mongoose.connection;
+
+// in case of any error in connection
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// tell us once connection with database was successful
+// db.once('open', function(){
+    // console.log('Connected to Database :: MongoDB');
+// });
+
+// exporting and make available
+// module.exports = db;
